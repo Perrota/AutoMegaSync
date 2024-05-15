01498b95e6670f9c749715d71b11e6145ff1a66d
Perrota <mrizzi48@gmail.com>
2024-05-15 12:59:55 -0300

    Implemented logging.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import time
 import sys
+import logging
 
 DEFAULT_EXE_PATH = r'C:\ProgramData\MEGAsync\MEGAsync.exe'
 
@@ -9,14 +10,19 @@ def create_cache_file(path:str) -> str:
     
     # Create if doesn't exist
     if not os.path.exists(path):
+        logger.info("Cache not found. Creating new.")
         with open(path, 'w') as f:
             f.write(DEFAULT_EXE_PATH)
+            logger.info(f"Cache created on {path}.")
     else:
         # Populate with default if exists but it's empty
         with open(path, 'r+') as f:
             contents = f.read()
             if contents == '':
+                logger.info("Empty cache found. Setting default.")
                 f.write(DEFAULT_EXE_PATH)
+            else:
+                logger.info("Existing cached value found.")
 
 def extract_valid_exe_path(cache_file_path:str) -> str:
     with open(cache_file_path, 'r') as f:
@@ -31,29 +37,39 @@ def extract_valid_exe_path(cache_file_path:str) -> str:
             with open(cache_file_path, 'w') as f:
                 f.write(exe_path)
 
+        logger.info("Valid path provided.")
         return exe_path
 
 def initialize_process(validated_path:str):
+    logger.info("Initializing process...")
     os.popen(validated_path)
 
 def terminate_process():
 
+    logger.info('Locating process to terminate...')
     subps = subprocess.Popen(['powershell', 'get-process'], stdout=subprocess.PIPE)
     output, _ = subps.communicate()
 
     for line in output.splitlines():
         if 'MEGAsync' in str(line):
             pid_int = int(line.split(None)[5])
+            logger.info(f'Process found at number {pid_int}.')
+            logger.info('Killing it.')
             os.kill(pid_int, 9)
+            logger.info('Process ended.')
     
 def wait_x_minutes(x_value:int):
+    logger.info("Awaiting program.")
     for x in range(x_value):
         time.sleep(60)
-        print(f'{x+1} minute(s) have passed.')
+        logger.info(f'{x+1} minute(s) have passed.')
 
 if __name__ == "__main__":
 
-    # Argument handler
+    # Initialize logger
+    logger = logging.getLogger(__name__)
+
+    # Argument handler - minutes
     run_minutes = 5
     if len(sys.argv) > 1:
         try:
@@ -61,6 +77,9 @@ if __name__ == "__main__":
             run_minutes = arg_minutes
         except:
             pass
+        if len(sys.argv) > 2:
+            if sys.argv[2] == '-v':
+                logging.basicConfig(level=logging.INFO)
         
     cache_path = os.path.join(os.path.dirname(__file__), 'cache.txt')
     create_cache_file(cache_path)
